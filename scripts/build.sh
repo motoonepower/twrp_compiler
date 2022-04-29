1b2e93208e9675e3e4a6a90c380a75dcabd0da7c
Rushi Ranpise <rushiranpise17@gmail.com>
2022-04-29 15:31:45 +0000

    inital build script

---
+++ scripts/build.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# Source Configs
+source $CONFIG
+
+# Change to the Source Directry
+cd /root/work
+
+# Set-up ccache
+if [ -z "$CCACHE_SIZE" ]; then
+    ccache -M 10G
+else
+    ccache -M ${CCACHE_SIZE}
+fi
+
+# Empty the VTS Makefile
+if [ -f frameworks/base/core/xsd/vts/Android.mk ]; then	# if [ -f frameworks/base/core/xsd/vts/Android.mk ]; then
+    rm -rf frameworks/base/core/xsd/vts/Android.mk && touch frameworks/base/core/xsd/vts/Android.mk	#     rm -rf frameworks/base/core/xsd/vts/Android.mk && touch frameworks/base/core/xsd/vts/Android.mk
+fi
+
+# Prepare the Build Environment
+source build/envsetup.sh
+
+# Run the Extra Command	
+$EXTRA_CMD
+
+# export some Basic Vars
+export ALLOW_MISSING_DEPENDENCIES=true
+export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1	
+export LC_ALL="C"
+
+# lunch the target
+if [ "$TWRP_BRANCH" = "twrp-11" ]; then
+    lunch twrp_${DEVICE}-eng || { echo "ERROR: Failed to lunch the target!" && exit 1; }
+else
+    lunch omni_${DEVICE}-eng || { echo "ERROR: Failed to lunch the target!" && exit 1; }
+fi
+
+
+# Build the Code
+if [ -z "$J_VAL" ]; then
+    mka -j$(nproc --all) $TARGET || { echo "ERROR: Failed to Build TWRP!" && exit 1; }
+elif [ "$J_VAL"="0" ]; then
+    mka $TARGET || { echo "ERROR: Failed to Build TWRP!" && exit 1; }
+else
+    mka -j${J_VAL} $TARGET || { echo "ERROR: Failed to Build TWRP!" && exit 1; }
+fi
+
+# Exit
+exit 0
